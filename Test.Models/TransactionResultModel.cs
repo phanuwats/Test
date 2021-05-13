9c9cf6f3206fd644631c374bee890080285e8aea
Phanuwats <phanuwats@hotmail.com>
2021-05-13 22:42:54 +0700

    Add project files.

---
+++ Test.Models/TransactionResultModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Test.Models
+{
+    public class TransactionResultModel
+    {
+        public string id { get; set; }
+        public string payment { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ Test.Services/ServiceBase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Test.Services
+{
+    public class ServiceBase
+    {
+        protected string con;
+
+        public ServiceBase()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+              .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+              .Build();
+
+            this.con = configuration["ConnectionString:DefaultConnection"];
+        }
+    }
+}
