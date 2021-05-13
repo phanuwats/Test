6d3dcc68199e1b052c0b4dc5f8336f164a9efc89
Phanuwats <phanuwats@hotmail.com>
2021-05-13 22:57:54 +0700

    Initial Model class

---
+++ Test.Models/PageResultModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test.Models
+{
+    public class PageResultModel
+    {
+        public string errorCode { get; set; }
+        public string errorMessage { get; set; }
+    }
+}
---
+++ Test.Models/TransactionModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test.Models
+{
+    public class TransactionModel
+    {
+        public string trnID { get; set; }
+        public string amount { get; set; }
+        public string currencyCode { get; set; }
+        public string trnDate { get; set; }
+        public string status { get; set; }
+
+        public bool isError { get; set; }
+        public string errorMessage { get; set; }
+    }
+}
